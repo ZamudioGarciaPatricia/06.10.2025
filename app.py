5029bb36a3ab4cd91150747372576c221cd31bd7
ZamudioGarciaPatricia <24308060610116@cetis61.edu.mx>
2025-10-08 09:13:33 -0600

    aa

--- app.py
+++ app.py
@@ -5,6 +5,22 @@ app = (__name__)
 @app.route('/')
 def index():
     return render_template('base.html')
+@app.route('/animales')
+def index():
+    return render_template('animales.html')
+
+@app.route('/carros')
+def index():
+    return render_template('base.html')
+
+@app.route('/acerca')
+def index():
+    return render_template('base.html')
+
+@app.route('/maravillas')
+def index():
+    return render_template('base.html')
+
 
 if __name__ =="__main__":
     app.run(debug=True)
