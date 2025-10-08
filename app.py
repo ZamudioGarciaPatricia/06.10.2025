865ea7d4169916f98e4da82fe884486be5b36032
ZamudioGarciaPatricia <24308060610116@cetis61.edu.mx>
2025-10-08 09:11:27 -0600

    aaa

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 from flask import Flask, render_template
+
+app = (__name__)
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+if __name__ =="__main__":
+    app.run(debug=True)
