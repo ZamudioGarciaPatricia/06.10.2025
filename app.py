a3c8741e7e51b0bae73ff6d3458fe39656270f4f
ZamudioGarciaPatricia <24308060610116@cetis61.edu.mx>
2025-10-08 18:14:37 -0700

    lsito

--- app.py
+++ app.py
@@ -1,28 +1,27 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+AUTOR = "patricia Zamudio Garcia"
 
 @app.route('/')
 def inicio():
-    autor = 'Zamudio Garcia Patricia'
-    return render_template('inicio.html', autor=autor)
+    return render_template('inicio.html', autor=AUTOR)
 
-
-@app.route('/animales')
+@app.route('/animales') 
 def animales():
-    return render_template('animales.html')
+    return render_template('animales.html', autor=AUTOR) 
 
-@app.route('/carros')
+@app.route('/carros') 
 def carros():
-    return render_template('base.html')
+    return render_template('carros.html', autor=AUTOR)
 
-@app.route('/acerca')
-def acerca():
-    return render_template('base.html')
-
-@app.route('/maravillas')
+@app.route('/maravillas') 
 def maravillas():
-    return render_template('base.html')
+    return render_template('maravillas.html', autor=AUTOR)
+
+@app.route('/acerca') 
+def acerca_de():
+    return render_template('acerca.html', autor=AUTOR)
 
 
 if __name__ =="__main__":
